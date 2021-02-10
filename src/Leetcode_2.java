e3cdaef0c6634bc97ed90fe87ca966a3813d22df
He Zhao <zh0208@163.com>
2021-02-10 21:25:26 +0800

    Leetcode #2 Medium 2020-2-10

--- src/Leetcode_2.java
+++ src/Leetcode_2.java
@@ -1,6 +1,11 @@
 import java.util.List;
 
-
+/***
+ * 2. Add Two Numbers
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
 
 //Time >99.94%
 //Memory > 13.1%
